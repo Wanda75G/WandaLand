89101763bb40ec9e67f9161d78cacbe9401513c3
Wanda75G <maria547galvan@gmail.com>
2026-04-15 00:28:11 -0600

    tu puedes

--- assets/p-CBzELu-H-legacy-BPMwHQWa.js
+++ assets/p-CBzELu-H-legacy-BPMwHQWa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CwDcdDlz.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.x}],execute:function(){
+System.register(["./index-legacy-Ce_kMJZj.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.x}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
